2d4ecd163589dd158173bfb2070177ae500cc106
nathanhleung <nathanhleung@users.noreply.github.com>
2025-07-30 16:33:15 -0700

    chore: set up vite config w/ ts declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "vitest/config";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
+    minify: false,
     lib: {
       entry: "src/index.ts",
       name: "jittered-fractional-indexing",
       formats: ["es"],
+      fileName: "index",
     },
     rollupOptions: {
       external: ["fractional-indexing"], // add deps to exclude here
     },
   },
+  plugins: [dts({ rollupTypes: true })],
   test: {
     environment: "node",
   },
